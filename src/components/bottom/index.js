8b80a630d2e5b964609125e9dc10176bb0612b7d
郭胜 <1535233734@qq.com>
2018-11-23 11:09:36 +0800

    【BDD-unit-testing】添加测试环境-23

--- src/components/bottom/index.js
+++ src/components/bottom/index.js
@@ -1,9 +1,4 @@
 export default function () {
-    var a = 1
-    var b = 1
-    if ((a + b) > 2) {
-      console.log('more than two')
-    }
     var element = document.createElement('h1')
     element.id = 'bottom'
     element.innerHTML = 'Hello Bottom!'
